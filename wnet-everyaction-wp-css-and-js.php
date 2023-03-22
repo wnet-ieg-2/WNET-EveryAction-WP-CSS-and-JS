61d3fcf9a10170da8d4c7d4d0f49f7c01bd5c1ef
William Tam <tamw@wnet.org>
2023-03-22 18:55:38 +0000

    properly enqueues scripts

--- wnet-everyaction-wp-css-and-js.php
+++ wnet-everyaction-wp-css-and-js.php
@@ -9,24 +9,21 @@ Authors: WNET Digital (plugin), MWD (JS and CSS)
 class WNET_EveryAction_WP_CSS_and_JS {
   private $dir;
   private $file;
-  private $assets_dir;
   public $assets_url;
   public $token;
   public $version;
 
   public function __construct() {
     $this->version = '0.1';
-    $this->dir = realpath( __DIR__ . '/..' );
-		$this->assets_dir = trailingslashit( $this->dir ) . 'assets';
-		$this->assets_url = trailingslashit(plugin_dir_url( __DIR__ ) ) . 'assets';
+    $this->assets_url = esc_url( trailingslashit( plugins_url( '/assets/', __FILE__ ) ) );
     $this->token = 'wnet_everyaction_wp';
   }
 
   public function enqueue_scripts() {
     // this will register and enqueue the assets in the specific template(s) that need them
-    wp_register_script( $this->token . '_js' , $this->assets_url . '/js/ea-donate-form.js', array('jquery'), $this->version, true );
+    wp_register_script( $this->token . '_js' , $this->assets_url . 'js/ea-donate-form.js', array('jquery'), $this->version, true );
     wp_enqueue_script( $this->token . '_js' );
-    wp_enqueue_style( $this->token . '_css', $this->assets_url . '/css/ea-donate-form-theme-styles.css', null, $this->version);
+    wp_enqueue_style( $this->token . '_css', $this->assets_url . 'css/ea-donate-form-theme-styles.css', null, $this->version);
   }
 
 
