e1f8fce2f71debc124829d0f4bce2bd6ee205188
Arturo Luna <art@arturoluna.com>
2023-06-21 16:20:59 -0700

    Updates to thirteen theme
    Adding ea-color-settings.css this file contains css variables that assign the frequency, amount and checkbox color styles.

--- assets/js/ea-donate-form.js
+++ assets/js/ea-donate-form.js
@@ -2,194 +2,146 @@
 var nvtag_callbacks = window.nvtag_callbacks = window.nvtag_callbacks || {};
 var nvtag_callbacks = nvtag_callbacks || {};
 nvtag_callbacks.postRender = nvtag_callbacks.postRender || [];
+nvtag_callbacks.cssPostRender = nvtag_callbacks.cssPostRender || [];
 nvtag_callbacks.alterFormDefinition = nvtag_callbacks.alterFormDefinition || [];
 window.nvtag_callbacks.alterRequireValid = window.nvtag_callbacks.alterRequireValid || [];
 window.nvtag_callbacks.alterErrors = window.nvtag_callbacks.alterErrors || [];
-
-var wnePostRender = function (args) {
-  $(".PremiumGift").append("<div id='gift-overlay'><div id='g-overlay-copy'></div></div>");
-  $("#g-overlay-copy").append("<h4>For Monthly Donors</h4>")
-  var applyCss;
-  var timeout;
-  $(document).scroll(function () {
-    var height = $(window).scrollTop();
-    // console.log( height)
-    if (height > 0) {
-      $("span span.text i").css("font-weight", "bold");
-      $("span span.text b").css("font-weight", "bold");
-    }
-  });
-
-  // Move First Checbox 
-  $("fieldset.at-fieldset.AdditionalInformation label.at-check").prependTo("fieldset.at-fieldset.ContactInformation")
-  $(".at-check").eq(0).css('margin', '0 10px');
-  $(".at-check").eq(1).css('margin', '0 10px');
-  $(".at-check").eq(2).css('margin', '0 10px');
-  function callTimeout() {
-    timeout = setTimeout(otherInput, 0);
-  }
-
-  function otherInput() {
-    $(".edit-otheramount").attr('placeholder', 'Other');
-  }
-
-  function callCSSStyles() {
-    applyCss = setTimeout(cssApplied, 0.50)
-  }
- 
-  function cssApplied() {
-    $("div.at-radio").css("margin", "0");
-  }
-  
-  var SelectedFrequencyBtn = function () {
-    cssApplied();
-    $("input[name=SelectedFrequency]").each(function () {
-      displayPremium()
-      if ($(this).is(':checked')) {
-        $(this).parent().css("background-color", "#007DA3")
-        $(this).parent().css("border", "2px solid #007DA3")
-        $(this).parent().css("color", "#ffffff")
-        callTimeout()
-      } else {
-        $(this).parent().css("background-color", "#ffffff")
-        $(this).parent().css("border", "2px solid #8f8f8f")
-        $(this).parent().css("color", "#000000")
-        callTimeout()
-      }
-    });
-  }
-
-  var selectedButton = function(){
-    cssApplied()
-    $("input[name=SelectAmount]").each(function () {
-      if ($(this).is(':checked')) {
-        $(this).parent().css("color", "#ffffff")
-        
-      } else {
-        $(this).parent().css("color", "#000000")
-      }
-    });
-  }
-
-  var displayPremium = function () {
-    selectedButton()
-    cssApplied()
-    var def = args.form_definition;
-    if (def.type !== 'ContributionForm') return;
-
-    var one_time = $("label.at-radio-label-0").children().is(':checked');
-    if (one_time) {
-      $("fieldset.at-fieldset.Premiums").hide();
-      $(".at-check").eq(0).hide();
-      $('#gift-0').click();
-      $('div#gift-overlay').show();
-    } else {
-      $("fieldset.at-fieldset.Premiums").show();
-      $(".at-check").eq(0).show();
-      $('div#gift-overlay').hide();
-    }
-  }
-
-  $(document).ready(function (e) {
-    // Apply True to gift, it's not checked even when set in page builder
-    $(function () {
-      var $radios = $('input:radio[name=gift]');
-      if ($radios.is(':checked') === false) {
-        $radios.filter('[value=0]').prop('checked', true);
-      }
-      // Same for Frequency
-      var $SelectedFrequency = $('input:radio[name="SelectedFrequency"]');
-      if ($SelectedFrequency.is(':checked') === false) {
-        $SelectedFrequency.filter('checked').prop('checked', true);
-      }
-      SelectedFrequencyBtn();
-      $("input[name=SelectedFrequency]").change(SelectedFrequencyBtn);
-    
-    });
-      // Modify title or Prefix and add to datalist
-      var dataList = jQuery('#at-prefixes');
-      var prefixAdd = ['Mrs.', 'Miss.', 'Dr.& Mrs.', 'Dr. & Mr.'];
-          dataList[0].options[1].value = "Mr.";
-          dataList[0].options[2].value = "Mr. & Mrs";
-          dataList[0].options[3].value = "Mr. & Mr.";
-          dataList[0].options[4].value = "Mrs. & Mrs.";
-          dataList[0].options[5].value = "Ms.";
-
-          $.each(prefixAdd, function (i, item) {
-          $("#at-prefixes").append($("<option>").attr('value', item).text(item));
-          });
-
-          $('input[name="Prefix"]').on('input', function() {
-            var input = $(this).val().toLowerCase();
-            var options = $('#at-prefixes').find('option').map(function() {
-              return $(this).val().toLowerCase();
-            }).get();
-            if ($.inArray(input, options) === -1) {
-              $(this).val('');
-            }
-          });
-
-    selectedButton();
-    callTimeout()
-    callCSSStyles()
-  });
-
-  // Page Builder Custom Footer CTA's links for Modals 
-  function hidePageModal(){
-    $('.w3-modal').hide();
-  }
-  $('span#footerCTA-1').click(function (e) { e.preventDefault(); $('div#cta-01').show(); });
-  $('span#footerCTA-2').click(function (e) { e.preventDefault(); $('div#cta-02').show(); });
-  $('.w3-button').click(function (e) { e.preventDefault(); hidePageModal(); });
-  return args;
-}
-
-
+window.nvtag_callbacks.onSubmit = window.nvtag_callbacks.onSubmit || [];
+window.nvtag_callbacks.postContributionAmountChanged = window.nvtag_callbacks.postContributionAmountChanged || [];
+var passPortOneTimeError = false;
+var upsellLB = true;
+var firedOnce = false;
+var applyCss;
+var timeout;
+var amnt;
 var showModal = false;
 var hideGiftAdd = true;
 var freq;
 var otherAmt;
+var passPortOtherNum = $("input[name=OtherAmount]").val()
 
-$(document).on('click','input[name="SelectAmount"]',function(){
+var cssPostRender = function( args ) {
+  $(document).ready(function (e) {
+    // Get and apply selected checked Frequency
+    var $SelectedFrequency = $('input:radio[name="SelectedFrequency"]');
+    if ($SelectedFrequency.is(':checked') === false) {
+      $SelectedFrequency.filter('checked').prop('checked', true);
+    }
 
-  var target = $(this).prop("defaultValue")
-  if(showModal){
-    if( target === '5.00' && freq === '4'){
-      $('#giftSug').show()
-     }
-     if(target === '10.00' && freq === '4'){
-      $('#giftSug').show()
-     }
-  }
-});
+    // Additional Questios -  Move Around Form.
+    $("fieldset.at-fieldset.AdditionalInformation label.at-check, fieldset.at-fieldset.AdditionalInformation label.at-text").prependTo("fieldset.at-fieldset.ContactInformation");
 
-$(document).on('click','#yesplease',function(e){
- e.preventDefault();
- $('#gift-9').click()
-});
+    $(".at-check").eq(0).css('margin', '0 10px');
+    $(".at-check").eq(1).css('margin', '0 10px');
+    $(".at-check").eq(2).css('margin', '0 10px');
+    $("div.at-radio").css("margin", "0");
+    $("div.at-radios.clearfix").css("margin", "0");
+    $(".edit-otheramount").attr('placeholder', 'Other');
 
-var checkFreqAndBtn = function(){
+    // Prefix Modify title or Prefix and add to datalist
+    var dataList = jQuery('#at-prefixes');
+    var prefixAdd = ['Mrs.', 'Miss.', 'Dr.& Mrs.', 'Dr. & Mr.'];
+        dataList[0].options[1].value = "Mr.";
+        dataList[0].options[2].value = "Mr. & Mrs";
+        dataList[0].options[3].value = "Mr. & Mr.";
+        dataList[0].options[4].value = "Mrs. & Mrs.";
+        dataList[0].options[5].value = "Ms.";
 
-  setTimeout(function() {
+        $.each(prefixAdd, function (i, item) {
+        $("#at-prefixes").append($("<option>").attr('value', item).text(item));
+        });
+
+        $('input[name="Prefix"]').on('input', function() {
+          var input = $(this).val().toLowerCase();
+          var options = $('#at-prefixes').find('option').map(function() {
+            return $(this).val().toLowerCase();
+          }).get();
+          if ($.inArray(input, options) === -1) {
+            $(this).val('');
+          }
+        }); 
+
+
+    return args;
+  });
+}
+
+// Frequency - One-Time or Monthly
+var SelectedFrequencyBtn = function() {
+  $("input[name=SelectedFrequency]").each(function () {
+    if ($(this).is(':checked')) {
+      $(this).parent().addClass( "freqChecked" )
+    } else {
+      $(this).parent().removeClass( "freqChecked" )
+    }
+    // Hide First Checbox From Additional Questions
+    if ($(this)[0].value === '0' && $(this).is(':checked') ) {
+      $(".at-check").eq(0).css('display', 'none');
+    } else {
+      $(".at-check").eq(0).css('display', 'block');
+    }
+    cssPostRender();
+    applySelectedColor();
+    displayPremium();
+  });
+}
+
+  var selectedButton = function(){
     $("input[name=SelectAmount]").each(function () {
       if ($(this).is(':checked')) {
-        $(this).parent().css("color", "#ffffff")
-        $("div.at-radio").css("margin", "0");
+        $(this).parent().addClass( "selectAmount" ) 
+        applySelectedColor();
       } else {
-        $(this).parent().css("color", "#000000")
+        $(this).parent().removeClass( "selectAmount" ) 
+        applySelectedColor();
       }
     });
-  }, 10);
-  $("div.at-radio").css("margin", "0");
+  }
+
+  var applySelectedColor = function(){
+    setTimeout(function() {
+      $("input[name=SelectAmount]").each(function () {
+        if ($(this).is(':checked')) {
+        $(this).parent().css("color", "#ffffff")
+        } else {
+        $(this).parent().css("color", "#000000")
+        }
+      });
+    }, 50);
+  }
+
+  // Page Builder Custom Footer CTA's links for Modals 
+  var hidePageModal = function(){
+    $('.w3-modal').hide();
+    $('.wne-modal').hide();
+    // This will reasign the passPortalModal and only show the suggestion modal one time
+    passPortModal = function(){}
+  }
+
+var passPortModal = function(amnt){
+  var shown = false; 
+  var otherAmount = amnt;
+  // console.log(otherAmount)
+  if(shown === false){
+    $("input[name=SelectedFrequency]").each(function () {
+      if ($(this)[0].value === '0' && $(this).is(':checked') ) {
+        $("input[name=SelectAmount]").each(function () {
+          if ((otherAmount <= 59.00) && $(this).is(':checked') ) {
+            $('#passPortAlertAmount').show();
+            shown = true;
+          } else {
+            $('#passPortAlertAmount').hide();
+          }
+        });
+      } 
+    });
+   }
 }
 
 var selectedGift = function () {
-
   if(hideGiftAdd === true){
     $("fieldset.ShippingInformation").hide();
-    // console.log($("fieldset.ShippingInformation"))
   }
-
   setTimeout(function() {
     $("input[name=gift]").each(function () {
       if ($(this).is(':checked')) {
@@ -201,82 +153,88 @@ var selectedGift = function () {
   }, 5);
 }
 
-$(document).on('click','.label-amount',function(){
-  checkFreqAndBtn();
- });
+var displayPremium = function () {
+  var def = args.form_definition;
+  if (def.type !== 'ContributionForm') return;
 
- $(document).on('change','.label-amount',function(){
-  checkFreqAndBtn();
- });
- 
- $(document).on('click','.at-radio-label-0',function(){
-  checkFreqAndBtn();
-});
+  var one_time = $("label.at-radio-label-0").children().is(':checked');
+  if (one_time) {
+    $("fieldset.at-fieldset.Premiums").hide();
+    $(".at-check").eq(0).hide();
+    $('#gift-0').click(); // click to no gift
+  } else {
+    $("fieldset.at-fieldset.Premiums").show();
+    $(".at-check").eq(0).show();
+  }
+}
 
-$(document).on('click','.at-radio-label-4',function(){
-  checkFreqAndBtn();
-  $('div#gift-overlay').hide();
-});
 
-$(document).on('click','input[name=SelectedFrequency]',function(){
-  checkFreqAndBtn();
-});
+var wnePostRender = function (args) {
+  $(document).on('click','.at-radio-label-0',function(){ SelectedFrequencyBtn(); });
+  $(document).on('click','.at-radio-label-4',function(){ SelectedFrequencyBtn(); });
+  $(document).on('click','.label-amount',function(){ selectedButton();});  
+  $(document).on('change','.label-amount',function(){ selectedButton();});
+  $(document).on('click','.label-amount',function(){ selectedButton();});  
+  $(document).on('change','.label-amount',function(){ selectedButton();});
+  $(document).on('click','.label-otheramount',function(){ selectedButton(); });
+  $(document).on('change','.label-otheramount',function(){ selectedButton(); });
+  $(document).on('change','.edit-otheramount',function(){ selectedButton(); });
+  $(document).on('click','.edit-otheramount',function(){ selectedButton(); });
+  $(document).on('change','.edit-otheramount',function(){ selectedButton(); });
+  $(document).on('focus','.edit-otheramount',function(){ selectedButton(); });
+  // Premium Gift Options
+  $(document).on('click','.at-gift',function(){ selectedGift(); });
+  $(document).on('change','.at-gift',function(){ selectedGift(); });
+  $(document).on('click','input[name="SelectAmount"]',function(){
+    var target = $(this).prop("defaultValue")
+    if(showModal){
+      if( target === '5.00' && freq === '4'){
+        $('#giftSug').show()
+       }
+       if(target === '10.00' && freq === '4'){
+        $('#giftSug').show()
+       }
+    }
+  });
+  
+  $(document).on('click','#yesplease',function(e){
+   e.preventDefault();
+   $('#gift-9').click()
+  });
 
-$(document).on('click','.label-otheramount',function(){
-  checkFreqAndBtn();
-});
-$(document).on('click','.edit-otheramount',function(){
-  checkFreqAndBtn();
-});
+  // Modals
+  $('span#footerCTA-1').click(function (e) { e.preventDefault(); $('div#cta-01').show(); });
+  $('span#footerCTA-2').click(function (e) { e.preventDefault(); $('div#cta-02').show(); });
+  $('.w3-button').click(function (e) { e.preventDefault(); hidePageModal(); });
+  $('.wne-button').click(function (e) { e.preventDefault(); hidePageModal(); });
 
-$(document).on('click','.gift-description',function(){
-  checkFreqAndBtn();
-});
+  SelectedFrequencyBtn();
+  applySelectedColor();
+  return args;
+} 
+// End Post Render
 
-$(document).on('click','.description',function(){
-  checkFreqAndBtn();
-});
-
-// $(document).on('click','.at-gift',function(){
-//   selectedGift();
-// });
-
-// $(document).on('change','.at-gift',function(){
-//   selectedGift();
-// });
-
-$(document).on('click','.edit-otheramount',function(){
-  checkFreqAndBtn();
-});
-
-$(document).on('change','.edit-otheramount',function(){
-  checkFreqAndBtn();
-});
-
-$(document).on('focus','.edit-otheramount',function(){
-  checkFreqAndBtn();
-});
-
-$(document).on('click','.label-otheramount-prefix',function(){
-  checkFreqAndBtn();
-});
-
-$(document).on('change','.label-otheramount-prefix',function(){
-  checkFreqAndBtn();
-});
-
-window.nvtag_callbacks.alterRequireValid.push(function(args) {
-  // selectedGift()
- freq = args.val;
+// Call passPortModal with value
+window.nvtag_callbacks.alterErrors.push(function (args) {
+  // console.log(args.def)
+  if(passPortOneTimeError === true){
+    if (args.field_name === 'SelectAmount') {
+      otherAmt = args.val
+      otherNumVal = parseInt(otherAmt)
+      $("input[name=SelectedFrequency]").each(function () {
+        if ($(this)[0].value === '0' && $(this).is(':checked') ) {
+          $("input[name=SelectAmount]").each(function () {
+            if (otherNumVal <= 59.99 ) {
+              passPortModal(otherNumVal)
+            } else {
+            }
+          });
+        } 
+      });
+    }
+  }
  return args;
 });
 
 nvtag_callbacks.postRender.push(wnePostRender);
-window.nvtag_callbacks.alterErrors.push(function (args) {
-    if (args.field_name === 'SelectAmount') {
-      otherAmt = args.val
-      checkFreqAndBtn();
-    }
-    return args;
-});
-
+nvtag_callbacks.postRender.push(cssPostRender);
