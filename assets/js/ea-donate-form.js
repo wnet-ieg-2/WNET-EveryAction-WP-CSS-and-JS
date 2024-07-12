8e411797ada8da4dcd78b4c2ceb33fbfc7fc72d6
Arturo Luna <120343+arturoluna@users.noreply.github.com>
2024-07-12 09:13:59 -0700

    Debug: Testing Update for checkboxes

--- assets/js/ea-donate-form.js
+++ assets/js/ea-donate-form.js
@@ -83,52 +83,14 @@ const initpostRender = function (args) {
       });
   });
   return args;
-}
+};
 
-// Show Passport Alert Amount Alert for One-Time Donation
-const passPortAlertAmount = function (amnt) {
-  let shown = false;
-  let otherAmount = amnt;
-  // console.log(otherAmount)
-  if (shown === false) {
-    $("input[name=SelectedFrequency]").each(function () {
-      if ($(this)[0].value === '0' && $(this).is(':checked')) {
-        $("input[name=SelectAmount]").each(function () {
-          if ((otherAmount <= 59.00) && $(this).is(':checked')) {
-            console.log(otherAmount)
-            $('#passPortAlertAmount').show();
-            shown = true;
-          } else {
-            $('#passPortAlertAmount').hide();
-          }
-        });
-      }
-    });
-  }
-}
-
-// Call passPortAlertAmount for Other OT value
-window.nvtag_callbacks.alterErrors.push(function (args) {
-   if (passPortOneTimeError === true) {
-    if (args.field_name === 'SelectAmount') {
-      // Other Amount
-      otherAmt = args.val
-      otherNumVal = parseInt(otherAmt)
-      $("input[name=SelectedFrequency]").each(function () {
-        if ($(this)[0].value === '0' && $(this).is(':checked') && otherNumVal !== 0) {
-          passPortAlertAmount(otherNumVal)
-        }
-      });
-    }
-   }
-  return args;
-});
-
-// Additional Questions
 // nvtag_callbacks.postRender
 nvtag_callbacks.postRender.push(editPrefix);
 nvtag_callbacks.postRender.push(initpostRender);
 
+// Additional Questions to move
+// Move I'm a current sustaining member and I'm updating my information
 const moveSustainerMemberField = function (args) {
   let formID;
   let additionalChildren = []; // Temporary array to store children from AdditionalInformation
@@ -172,17 +134,189 @@ const moveSustainerMemberField = function (args) {
     handleFrequencyChange();
     // Bind change event
     $('input[name="SelectedFrequency"]').on('change', handleFrequencyChange);
-
-    console.log(formID)
-    console.log(additionalChildren[0])
-    console.log(additionalChildren[0].name)
-    console.log(sustainerMember)
-  });
+  }); // End of document ready
 
   return args;
 };
 
+// Move Yes, I would like to receive emails from WNET/ALL ARTS
+const moveRecieveEmailsField = function(args){
+  let formID;
+  let additionalChildren = []; // Temporary array to store children from AdditionalInformation
+  if (args && args.form_definition ) {
+    formID = args.form_definition.formId;
+  }
+  _.each(args.form_definition.form_elements, function (child) {
+    if (child.name === 'AdditionalInformation') {
+      // Store children of AdditionalInformation
+      additionalChildren = child.children.slice(); // Use slice to copy the array
+    }
+  });
+
+  $(document).ready(function (){
+    let receiveEmails = $('.'+ additionalChildren[1].name);
+    let targetField = $('#NVContributionForm' + formID + '-ContactInformation .at-fields');
+
+    if (receiveEmails.length && targetField.length) {
+        receiveEmails.insertAfter(targetField);
+     }
+  }); // End of document ready
+
+  return args;
+};
+
+// Move Yes, I would like to receive texts from WNET/ALL ARTS
+const moveReceiveTextsField = function(args){
+  let formID;
+  let additionalChildren = []; // Temporary array to store children from AdditionalInformation
+  if (args && args.form_definition ) {
+    formID = args.form_definition.formId;
+  }
+  _.each(args.form_definition.form_elements, function (child) {
+    if (child.name === 'AdditionalInformation') {
+      // Store children of AdditionalInformation
+      additionalChildren = child.children.slice(); // Use slice to copy the array
+    }
+  });
+
+  $(document).ready(function (){
+    let receiveTexts = $('.'+ additionalChildren[2].name);
+    let targetField = $('#NVContributionForm' + formID + '-ContactInformation .at-fields');
+
+    if (receiveTexts.length && targetField.length) {
+        receiveTexts.insertAfter(targetField);
+     }
+
+  }); // End of document ready
+
+  return args;
+};
+
+// Yes, I would like to be connected to my local PBS station
+const moveBeConnectedField = function(args){
+  let formID;
+  let additionalChildren = []; // Temporary array to store children from AdditionalInformation
+  if (args && args.form_definition ) {
+    formID = args.form_definition.formId;
+  }
+  _.each(args.form_definition.form_elements, function (child) {
+    if (child.name === 'AdditionalInformation') {
+      // Store children of AdditionalInformation
+      additionalChildren = child.children.slice(); // Use slice to copy the array
+    }
+  });
+
+  $(document).ready(function (){
+    let beConnected = $('.'+ additionalChildren[3].name);
+    let targetField = $('#NVContributionForm' + formID + '-ContactInformation .at-fields');
+
+    if (beConnected.length && targetField.length) {
+        beConnected.insertAfter(targetField);
+     }
+    // console.log(formID)
+    // console.log(additionalChildren[3])
+    // console.log(additionalChildren[3].name)
+    // console.log(additionalChildren[3].title)
+    // console.log(beConnected)
+  }); // End of document ready
+
+  return args;
+};
+
+const updateAutoRenewalSubscriptionCopy = function (args) {
+  // Update the text for the auto renewal subscription
+  let formID;
+  let additionalChildren = []; // Temporary array to store children from AdditionalInformation
+  if (args && args.form_definition ) {
+    formID = args.form_definition.formId;
+  }
+  _.each(args.form_definition.form_elements, function (child) {
+    if (child.name === 'AdditionalInformation') {
+      // Store children of AdditionalInformation
+      additionalChildren = child.children.slice(); // Use slice to copy the array
+    }
+  });
+
+  $(document).ready(function (){
+    let emailCopy = `or emailing <a href='mailto:membership@wnet.org'>membership@wnet.org</a>.`;
+    let urlAdd = `You also acknowledge and agree to the full Terms of Service located at <a href='https://www.wnet.org/about/terms-of-service/' target='_blank'>https://www.wnet.org/about/terms-of-service/</a>`;
+    let mainCopy = $('#'+ 'NVContributionForm' + formID +'-AdditionalInformation-' +  additionalChildren[4].name + '-label').html();
+    let label = $('#'+ 'NVContributionForm' + formID +'-AdditionalInformation-' +  additionalChildren[4].name + '-label');
+    // Update the HTML content of the label element
+    label.html(mainCopy+ ' '+ emailCopy + ' ' + urlAdd);
+    }); // End of document ready
+
+  return args;
+};
+
+// Handle Auto Renewal Subscription Checkbox
+window.nvtag_callbacks.alterRequireValid.push(function (args) {
+ 
+  // let formID;
+  // let additionalChildren = []; // Temporary array to store children from AdditionalInformation
+  // if (args && args.form_definition ) {
+  //   formID = args.form_definition.formId;
+  // }
+
+  // _.each(args.form_definition.form_elements, function (child) {
+  //   if (child.name === 'AdditionalInformation') {
+  //     // Store children of AdditionalInformation
+  //     additionalChildren = child.children.slice(); // Use slice to copy the array
+  //   }
+  // });
+
+      // console.log(formID)
+    // console.log(additionalChildren[3])
+    // console.log(additionalChildren[3].name)
+    // console.log(additionalChildren[3].title)
+    // console.log(beConnected)
+
+  // console.log(args)
+  // let autoRenewalSubscriptionCheckbox = $('input[name=' + additionalChildren[4].name + ']').closest('.at-row');
+  // let fieldSet = autoRenewalSubscriptionCheckbox.children();
+
+  // $("input[name=SelectedFrequency]").each(function () {
+  //   // One-Time
+  //   if ($(this)[0].value === '0' && $(this).is(':checked')) {
+  //     if (fieldSet[0]) {
+  //     let fieldStatus = fieldSet[0].children[0].checked;
+  //       fieldSet[0].style.display = 'none';
+  //       fieldSet[0].children[0].checked = false; // Uncheck the checkbox
+  //       $('input.at-submit.btn-at.btn-at-primary').prop('disabled', false);
+  //       }
+  //   }
+  //   // Monthly
+  //   if ($(this)[0].value === '4' && $(this).is(':checked')) {
+  //     if (fieldSet[0]) {
+  //     let fieldStatus = fieldSet[0].children[0].checked;
+  //         fieldSet[0].style.display = 'block';
+  //       if(fieldStatus === false) {
+  //        $('input.at-submit.btn-at.btn-at-primary').prop('disabled', true);
+  //       }
+  //     }
+  //   }
+
+  // })
+
+  // function handleCheckBox() {
+  //   let isChecked = $(this)[0].children[0].checked;
+  //   if (isChecked === false) {
+  //     $('input.at-submit.btn-at.btn-at-primary').prop('disabled', true);
+  //   } else {
+  //     $('input.at-submit.btn-at.btn-at-primary').prop('disabled', false);
+  //   }
+  // }
+
+  // $('label.' + additionalChildren[4].name).on('click', handleCheckBox)
+  console.log(args)
+  return args;
+})
+
+nvtag_callbacks.postRender.push(moveBeConnectedField);
+nvtag_callbacks.postRender.push(moveReceiveTextsField);
+nvtag_callbacks.postRender.push(moveRecieveEmailsField);
 nvtag_callbacks.postRender.push(moveSustainerMemberField);
+nvtag_callbacks.postRender.push(updateAutoRenewalSubscriptionCopy);
 
 // nvtag_callbacks.postRender.push(addAutoRenewalSubscriptionCopy);
 // nvtag_callbacks.alterFormDefinition.push(getAdditionalQuestions);
@@ -190,3 +324,44 @@ nvtag_callbacks.postRender.push(moveSustainerMemberField);
 // nvtag_callbacks.alterFormDefinition.push(moveBeConnectedField);
 // nvtag_callbacks.alterFormDefinition.push(moveReceiveTextsField);
 // nvtag_callbacks.alterFormDefinition.push(addReceiveEmailsField);
+
+// Show Passport Alert Amount Alert for One-Time Donation
+const passPortAlertAmount = function (amnt) {
+  let shown = false;
+  let otherAmount = amnt;
+  // console.log(otherAmount)
+  if (shown === false) {
+    $("input[name=SelectedFrequency]").each(function () {
+      if ($(this)[0].value === '0' && $(this).is(':checked')) {
+        $("input[name=SelectAmount]").each(function () {
+          if ((otherAmount <= 59.00) && $(this).is(':checked')) {
+            console.log(otherAmount)
+            $('#passPortAlertAmount').show();
+            shown = true;
+          } else {
+            $('#passPortAlertAmount').hide();
+          }
+        });
+      }
+    });
+  }
+}
+
+
+// Call passPortAlertAmount for Other OT value
+window.nvtag_callbacks.alterErrors.push(function (args) {
+   if (passPortOneTimeError === true) {
+    if (args.field_name === 'SelectAmount') {
+      // Other Amount
+      otherAmt = args.val
+      otherNumVal = parseInt(otherAmt)
+      $("input[name=SelectedFrequency]").each(function () {
+        if ($(this)[0].value === '0' && $(this).is(':checked') && otherNumVal !== 0) {
+          passPortAlertAmount(otherNumVal)
+        }
+      });
+    }
+   }
+  // console.log(args)
+  return args;
+});
