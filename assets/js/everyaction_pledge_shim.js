03d3c315f4c235ed88a9663334e443d3a3eac6fa
William Tam <tamw@wnet.org>
2024-12-18 16:07:28 -0500

    tweaks to pledge shim

--- assets/js/everyaction_pledge_shim.js
+++ assets/js/everyaction_pledge_shim.js
@@ -20,7 +20,7 @@
 		premium_field = $("textarea[title='Your Selected Premiums']");
 		$(pcode_field).parent().parent().hide();
 		$(premium_field).parent().parent().hide();
-		$(premium_field).parent().parent().parent().append('<div class="premium_list"></div>');
+		$('header.at-markup.HeaderHtml').append('<div class="premium_list"></div>');
 	}
 
 
@@ -34,14 +34,14 @@
 		required_donation_amount = 0;
 		$.each(shopping_cart, function( index, item ) {
 			pcodestring += item.pcode + ':' + item.quantity + '|';
-			var this_item_value = Number(item.quantity) + Number(item.required_amount);
+			var this_item_value = Number(item.quantity) * Number(item.required_amount);
 			premiumstring += item.title + ' | Premium Code: ' + item.pcode + ' | Price: $' + item.required_amount + ' | Quantity: ' +  item.quantity + "\n" ;
 			premiumhtml += '<li><strong>' + item.title + '</strong> | Premium Code: ' + item.pcode + ' | Price: $' + item.required_amount + ' | Quantity: ' +  item.quantity + "</li>" ;
 			required_donation_amount += this_item_value;
 		});
 		pcode_field.val(pcodestring);
 		premium_field.val(premiumstring);
-		$('.premium_list').html('<h4>Your order: <em><a href="/support/pledge-premiums/cart/">edit items</a></em></h4><ul>' + premiumhtml + '</ul>');
+		$('.premium_list').html('<h4>Your order: <em><a href="/support/pledge-premiums/cart/">edit items</a></em></h4><ul>' + premiumhtml + '</ul><p class="contribution_message">Minimum contribution: $<span class="required_contribution">'+required_donation_amount+'</span></p>');
 		set_required_amount();
 	}
 
--- wnet-everyaction-wp-css-and-js.php
+++ wnet-everyaction-wp-css-and-js.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WNET EveryAction WP CSS and JS 
 Description: A container to maintain the custom JS and CSS for embedded EveryAction forms on WNET sites
-Version: 2024.12.12
+Version: 2024.12.18
 Authors: WNET Digital (plugin), MWD (JS and CSS)
 */
 
@@ -14,7 +14,7 @@ class WNET_EveryAction_WP_CSS_and_JS {
   public $version;
 
   public function __construct() {
-    $this->version = '2024.12.05'; 
+    $this->version = '2024.12.18'; 
     /* use today's date in the line above in 'xxxx.xx.xx' format to make sure CloudFront is using this version. 
      * If including the script elsewhere, it is generally best to also include that date as a query string
     */   
