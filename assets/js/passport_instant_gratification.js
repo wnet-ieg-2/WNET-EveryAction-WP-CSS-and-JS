dad1a01ff034ddf23bbd46cf8cbf9f392a079b1f
William Tam <tamw@wnet.org>
2026-01-29 09:54:30 -0500

    move external JS into assets/js and localize the passport instant grat script

---
+++ assets/js/passport_instant_gratification.js
@@ -0,0 +1,147 @@
+/******************
+******************
+******************
+***
+*** 2026_MWD_WNET_Passport_Instant_Gratification.js
+***
+******************
+******************
+******************/
+// Ensure nvtag_callbacks.preSegue exists
+nvtag_callbacks.preSegue = nvtag_callbacks.preSegue || [];
+
+// @ts-ignore
+const passPortInstantGratification = function (args) {
+
+    console.log('preSegue: passPortInstantGratification', args);
+
+	// this line here takes the value from WP's localize_script function to assign station.station_name
+    var station_nice_name = station.station_name;
+
+	// these lines now use server-relative urls, since they're the same on all WNET station sites
+    var instant_grat_ajaxurl = '/pbsoauth/instant_gratification/';
+    var activateurl = '/pbsoauth/activate/?activation_token=';
+    var watchurl = '/programs/';
+    var imgurl = '/wp-content/plugins/wnet-passport-instant-gratification/assets/img/';
+
+
+  function createProvisionalMembership() {
+    $('#mvault_status_window').html('<div class="loading"><p><img src="' + imgurl + 'loading.gif" style="width:1em;" />&nbsp;Creating ' + station_nice_name + ' Passport Account...</p></div>');
+    // @ts-ignore
+    var ajax = $.ajax({
+      type: "POST",
+      url: instant_grat_ajaxurl,
+      data:{
+        'trans_id': trans_id,
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'from_pbs' : pbs_referrer_qs,
+        'xv' : 'skip' 
+      },
+      xhr: function() {
+        var xhr = $.ajaxSettings.xhr();
+        return xhr;
+      },
+      dataType: "json"
+    }).done(function(response) {
+      console.log(response);
+      var responsetxt; 
+      if (typeof response.token !== 'undefined') {
+        responsetxt = '<div class="activate"><a href="' + activateurl + response.token + pbs_referrer_qs + '" target=_new><img style="display:inline;" src="' + imgurl + 'passport_icon.png">&nbsp;Your donation qualifies for Passport, our on-demand member benefit. Click here now to activate your '+ station_nice_name + ' Passport benefit!</a></div><p><i>Not ready to activate right now?  Look for an email from us with your activation code, or you can save this code for later:</i> <b>' + response.token + '</b></p>';
+      } else if (response.activated == true) {
+        responsetxt = '<div class="login"><a href="' + watchurl + '"><img style="display:inline;" src="' + imgurl + 'passport_icon.png">&nbsp;You have already activated your '+ station_nice_name + ' Passport access. Click here to login</a></div>';
+      } else {
+        if (typeof response.errors !== 'undefined') {
+          if (response.errors == 'multiple accounts') {
+            responsetxt = "<p>There is already an account on record with your email address that has access to " + station_nice_name + " Passport, and you should already have received an email with your login info.</p>";
+          } else {
+            responsetxt = '<div class="error"><img align="left" src="' + imgurl + 'passport_icon.png">&nbsp;We were unable to create your '+ station_nice_name + ' Passport account because of the following error: <br />' + response.errors + "</div>";
+          }
+        }
+      }
+      $('#mvault_status_window').html(responsetxt);
+    // @ts-ignore
+    }).fail(function(response) {
+      $('#mvault_status_window').html('<div class="error">Could not create '+ station_nice_name + ' Passport Account!</div>');
+    });
+  }
+
+  function checkForAmountThenCreateMember() {
+    // Luminate doesn't provide a decent way to filter for amount.
+    var amount = 0;
+    if ( $('#trans_amount').length ) {
+      // @ts-ignore
+      amount = $('#trans_amount').text();
+      // @ts-ignore
+      amount = Number(amount.replace(/[^0-9\.]+/g,""));
+    }
+    // sustainers have a frequency unit of 'months', all others dont
+    if ( $('#trans_frequency_unit').length ) {
+      // @ts-ignore
+      freq = $('#trans_frequency_unit').text();
+      // @ts-ignore
+      if ((freq == 'months') || (freq == 'monthly')) {
+        amount = amount * 12;
+      } 
+    }
+    console.log('calculated amount: ' + amount);
+    if (amount && (amount < 60) ) {
+      console.log('Amount below threshold of 60: ' + amount);
+      $('#mvault_status_window').html('<p><i>Note: Due to rights restrictions, '+ station_nice_name + ' Passport is only available to Members who have given $60 or more in the past year.</i></p>');
+    } else {
+      // passed our tests, so it is worth trying to attemp the membership 
+      createProvisionalMembership();
+    }
+  }
+
+
+  /* these are spans on the thankyou page */
+  var trans_id = $('#transaction_id').text();
+  var first_name = $('#trans_first_name').text();
+  var last_name = $('#trans_last_name').text();
+  var email = $('#trans_email').text();
+  var xv = 1;
+  if ( $('#trans_xv').length ) {
+    // @ts-ignore
+    xv = $('#trans_xv').text();
+  } 
+   
+  /* this if for the connectivity tester and doesnt check amount */ 
+  $('#InstantGratTester').click(function(e){
+    e.preventDefault();
+    // @ts-ignore
+    trans_id = $('#transaction_id').val();
+    // @ts-ignore
+    first_name = $('#trans_first_name').val();
+    // @ts-ignore
+    last_name = $('#trans_last_name').val();
+    // @ts-ignore
+    email = $('#trans_email').val();
+    // @ts-ignore
+    xv = $('#trans_xv').val();
+    createProvisionalMembership();
+  });
+
+  /* this checks for amount first */
+  $(function() {
+    if ( ! $('#InstantGratTester').length){ 
+      checkForAmountThenCreateMember();
+    }
+  });
+
+  // handling the PBS return path var
+  // @ts-ignore
+  if (typeof pbs_referrer === 'undefined') {
+    var pbs_referrer_qs = '';
+  } else {
+    // @ts-ignore
+    var pbs_referrer_qs = '/?return_path=' + pbs_referrer;
+  }
+
+  return args;
+
+};
+
+// nvtag_callbacks.postRender
+nvtag_callbacks.preSegue.push(passPortInstantGratification);
--- wnet-everyaction-wp-css-and-js.php
+++ wnet-everyaction-wp-css-and-js.php
@@ -14,7 +14,7 @@ class WNET_EveryAction_WP_CSS_and_JS {
   public $version;
 
   public function __construct() {
-    $this->version = '2026.01.28.1'; 
+    $this->version = '2026.01.29'; 
     /* use today's date in the line above in 'xxxx.xx.xx' format to make sure CloudFront is using this version. 
      * If including the script elsewhere, it is generally best to also include that date as a query string
     */   
@@ -27,16 +27,23 @@ class WNET_EveryAction_WP_CSS_and_JS {
 
 	if ( is_singular('contribute')) {
         global $post;
+
+		// set $station_name from the theme
+		$station_name = !empty(STATION_THEME) ? STATION_THEME : 'THIRTEEN';
+
+
+
 		wp_register_script( $this->token . '_js' , $this->assets_url . 'js/ea-donate-form.js', array('jquery'), $this->version, true );
 		wp_enqueue_script( $this->token . '_js' );
 		wp_script_add_data($this->token . '_js',  'data-script-justification', 'Support file required for WNET-EveryAction-WP-CSS-and-JS plugin.');
 		wp_enqueue_style( $this->token . '_css', $this->assets_url . 'css/ea-donate-form-theme-styles.css', null, $this->version);
-		wp_register_script( $this->token . '_passportinstantgrat_js', 'https://nvlupin.blob.core.windows.net/images/van/WNET/WNET/1/78997/images/2025_MWD_WNET_Contribution_Main_Theme/js/reusables/2026_MWD_WNET_Passport_Instant_Gratification.js', array('jquery'), $this->version, true );
+		wp_register_script( $this->token . '_passportinstantgrat_js', $this->assets_url . 'js/passport_instant_gratification.js', array('jquery'), $this->version, true );
 		wp_enqueue_script( $this->token . '_passportinstantgrat_js');
+		wp_localize_script( $this->token . '_passportinstantgrat_js', 'station', array('station_name' => $station_name) ); 
 		wp_script_add_data( $this->token . '_passportinstantgrat_js', 'data-script-justification', 'Support file required for WNET-EveryAction-WP-CSS-and-JS plugin.');
 		if ('premium-checkout' == $post->post_name) {
 			// enqueue the pledge shim and pledge additional questions only on the pledge premium checkout form 
-			wp_register_script( $this->token . '_ea_additionalfields_premiums_js', 'https://nvlupin.blob.core.windows.net/images/van/WNET/WNET/1/78997/images/2025_MWD_WNET_Contribution_Main_Theme/js/reusables/2026_WNET_Main_Theme_AQ_E-Store.js', array($this->token . '_js'), $this->version, true );
+			wp_register_script( $this->token . '_ea_additionalfields_premiums_js', $this->assets_url . 'js/additional_questions_estore.js', array($this->token . '_js'), $this->version, true );
             wp_enqueue_script( $this->token . '_ea_additionalfields_premiums_js');
             wp_script_add_data( $this->token . '_ea_additionalfields_premiums_js',  'data-script-justification', 'Support file required for WNET-EveryAction-WP-CSS-and-JS plugin.');
             wp_register_script('everyaction_pledge_shim',$this->assets_url . 'js/everyaction_pledge_shim.js', array('jquery'), $this->version, true);
@@ -44,7 +51,7 @@ class WNET_EveryAction_WP_CSS_and_JS {
 			wp_script_add_data( 'everyaction_pledge_shim',  'data-script-justification', 'Support file required for WNET-EveryAction-WP-CSS-and-JS plugin.');
 		} else {
 			// all the other everyaction donate forms get these additional questions
-			wp_register_script( $this->token . '_ea_additionalfields_js', 'https://nvlupin.blob.core.windows.net/images/van/WNET/WNET/1/78997/images/2025_MWD_WNET_Contribution_Main_Theme/js/reusables/2026_WNET_Main_Theme_AQ_Standard.js', array($this->token . '_js'), $this->version, true );
+			wp_register_script( $this->token . '_ea_additionalfields_js', $this->assets_url . 'js/additional_questions_standard.js', array($this->token . '_js'), $this->version, true );
 			wp_enqueue_script( $this->token . '_ea_additionalfields_js');
 			wp_script_add_data( $this->token . '_ea_additionalfields_js',  'data-script-justification', 'Support file required for WNET-EveryAction-WP-CSS-and-JS plugin.');
 		}
